5dbbd815485e385bae3d302d7bd8d667dd21b7da
michaelprince3 <michaelprince3@gmail.com>
2020-06-04 11:43:31 -0500

    APImanager complete

--- src/modules/ApiManager.js
+++ src/modules/ApiManager.js
@@ -6,9 +6,53 @@ export default {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
+        "Authorization": `Token ${sessionStorage.getItem("ecommerceapi-token")}`
       },
-    });
+    })
+    .then(response => response.json())
   },
 
-  
+  getAll(route) {
+    return fetch(`${apiUrl}/${route}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Token ${sessionStorage.getItem("ecommerceapi-token")}`
+      },
+    })
+    .then(response => response.json())
+  },
+
+  create(route, createItem) {
+    return fetch(`${apiUrl}/${route}`, {
+      "method": "POST",
+      "headers": {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": `Token ${sessionStorage.getItem("ecommerceapi-token")}`
+      },
+      body: JSON.stringify(createItem)
+    })
+  },
+
+  update(route, id, updateItem) {
+      return fetch(`${apiUrl}/${route}/${id}`, {
+        "method": "PUT",
+        "headers": {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          "Authorization": `Token ${sessionStorage.getItem("ecommerceapi-token")}`
+        },
+        body:JSON.stringify(updateItem)
+      })
+  },
+
+  delete(route, id) {
+    return fetch(`${apiUrl}/${route}/${id}`, {
+        "method": "DELETE",
+        "headers": {
+          "Authorization": `Token ${sessionStorage.getItem("ecommerceapi-token")}`
+        }
+      })
+  }
 };
