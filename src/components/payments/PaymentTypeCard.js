0f0e8f64e8d0c3e24e07a681beb2f4eed0525b4c
Tyler Simeone <tyler.simeone@gmail.com>
2020-06-05 15:02:54 -0500

    payment type now displaying data for payment methods

--- src/components/payments/PaymentTypeCard.js
+++ src/components/payments/PaymentTypeCard.js
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 
 const PaymentTypeCard = (props) => {
     return (
-        <div>
+        <div className="paymentTypeCard">
             <h2>Merchant Name: {props.paymentMethod.merchant_name}</h2>
+            <p>Card Number: {props.paymentMethod.account_number}</p>
+            <p>Expiration Date: {props.paymentMethod.expiration_date}</p>
         </div>
     )
 }
