ebdf2be82fd5adf08c36e1430a97bda3e77f980a
Tyler Simeone <tyler.simeone@gmail.com>
2020-06-05 15:14:44 -0500

    can now delete a payment type

--- src/components/payments/AddPayment.js
+++ src/components/payments/AddPayment.js
@@ -29,7 +29,8 @@ const AddPayment = (props) => {
         };
 
         ApiManager.create("paymenttypes", paymentTypeCopy)
-            .then(ApiManager.getAll("paymenttypes").then(resp => setPaymentMethods(resp)))
+            .then(ApiManager.getAll("paymenttypes")
+            .then(resp => setPaymentMethods(resp)))
   };
 
   useEffect(() => {
--- src/components/payments/PaymentTypeCard.js
+++ src/components/payments/PaymentTypeCard.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import ApiManager from "../../modules/ApiManager";
 
 const PaymentTypeCard = (props) => {
+    const handleDelete = () => {
+        ApiManager.delete("paymenttypes", props.paymentMethod.id)
+            .then(props.history.push("/account"));
+    }
+
     return (
         <div className="paymentTypeCard">
             <h2>Merchant Name: {props.paymentMethod.merchant_name}</h2>
             <p>Card Number: {props.paymentMethod.account_number}</p>
             <p>Expiration Date: {props.paymentMethod.expiration_date}</p>
+            <button onClick={handleDelete}>Delete Payment Method</button>
         </div>
     )
 }
