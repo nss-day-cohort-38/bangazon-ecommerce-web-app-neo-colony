f05d4124cb142cd7ee3542d3060244e1e0ef8974
Tyler Simeone <tyler.simeone@gmail.com>
2020-06-05 13:29:09 -0500

    working on fetching list of payment types to add to payment type card to delete

--- src/components/payments/AddPayment.js
+++ src/components/payments/AddPayment.js
@@ -7,6 +7,7 @@ const AddPayment = (props) => {
     account_number: "",
     expiration_date: "",
   });
+  const [paymentMethods, setPaymentMethods] = useState([])
   const [isLoading, setIsLoading] = useState(false);
 
   const handleFieldChange = (evt) => {
@@ -26,52 +27,57 @@ const AddPayment = (props) => {
       expiration_date: exp_date,
     };
 
-    ApiManager.create("paymenttypes", paymentTypeCopy).then(() => {
-        props.history.push("/account")
-    })
+    ApiManager.create("paymenttypes", paymentTypeCopy)
+        .then(ApiManager.getAll("paymenttypes"))
+        .then(() => {props.history.push("/account")})
   };
 
   return (
-    <form className="form--create-paymenttype" onSubmit={addPaymenttype}>
-      <h1 className="h3 mb-3 font-weight-normal">Add a payment method</h1>
-      <fieldset>
-        <label htmlFor="merchant_name"> Merchant Name </label>
-        <input
-          onChange={handleFieldChange}
-          type="text"
-          id="merchant_name"
-          placeholder="Merchant Name"
-          required=""
-          autoFocus=""
-        />
-      </fieldset>
-      <fieldset>
-        <label htmlFor="account_number"> Account Number </label>
-        <input
-          onChange={handleFieldChange}
-          type="text"
-          id="account_number"
-          placeholder="Ex: 5555 5555 5555 5555"
-          required=""
-          autoFocus=""
-        />
-      </fieldset>
-      <fieldset>
-        <label htmlFor="expiration_date"> Expiration Date </label>
-        <input
-          onChange={handleFieldChange}
-          type="date"
-          id="expiration_date"
-          required=""
-          autoFocus=""
-        />
-      </fieldset>
-      <fieldset>
-        <button disabled={isLoading} type="submit">
-          Submit
-        </button>
-      </fieldset>
-    </form>
+    <>
+        <form className="form--create-paymenttype" onSubmit={addPaymenttype}>
+        <h1 className="h3 mb-3 font-weight-normal">Add a payment method</h1>
+        <fieldset>
+            <label htmlFor="merchant_name"> Merchant Name </label>
+            <input
+            onChange={handleFieldChange}
+            type="text"
+            id="merchant_name"
+            placeholder="Merchant Name"
+            required=""
+            autoFocus=""
+            />
+        </fieldset>
+        <fieldset>
+            <label htmlFor="account_number"> Account Number </label>
+            <input
+            onChange={handleFieldChange}
+            type="text"
+            id="account_number"
+            placeholder="Ex: 5555 5555 5555 5555"
+            required=""
+            autoFocus=""
+            />
+        </fieldset>
+        <fieldset>
+            <label htmlFor="expiration_date"> Expiration Date </label>
+            <input
+            onChange={handleFieldChange}
+            type="date"
+            id="expiration_date"
+            required=""
+            autoFocus=""
+            />
+        </fieldset>
+        <fieldset>
+            <button disabled={isLoading} type="submit">
+            Submit
+            </button>
+        </fieldset>
+        </form>
+        <div>
+            {}
+        </div>
+    </>
   );
 };
 
---
+++ src/components/payments/PaymentTypeCard.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const PaymentTypeCard = (props) => {
+    return (
+        <div>
+            <h2>Merchant Name: {}</h2>
+        </div>
+    )
+}
