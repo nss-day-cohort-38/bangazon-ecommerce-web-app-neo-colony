bea6535a5ac105e48310fbe075b9048201246975
Michael Clark <michaelclarknashville@protonmail.com>
2020-06-07 12:20:14 -0500

    categories page shows first 3 products, this branch also includes product details fix

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,29 +1,29 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import ApiManager from '../../modules/ApiManager';
 import CategoryCard from './CategoryCard';
 
 const Categories = props => {
 
-    const [fetched, setFetched] = useState("")
-    
+    const [fetched, setFetched] = useState([])
 
-    const fetchCategories = () => {
-        ApiManager.getAll("producttypes")
-        .then(resp => {
-            console.log(resp, "RESP")
-            setFetched(resp)
-            console.log(fetched)
-        })
-    }
 
     useEffect(() => {
-        fetchCategories()
+        // fetchCategories();
+        ApiManager.getAll("producttypes")
+            .then(resp => {
+
+                setFetched(resp)
+
+                console.log(resp, "RESP, LINE 16")
+                console.log(fetched, "LINE 17")
+            })
     }, [])
 
     return (
         <div>
-            {/* {fetched.map(res => <CategoryCard category={res} key={res.id}/>)} */}
-            </div>
+            {fetched.map(res => <CategoryCard category={res} key={res.id} />)}
+            {/* {fetched[0].name} */}
+        </div>
     )
 
 }
--- src/components/categories/CategoryCard.js
+++ src/components/categories/CategoryCard.js
@@ -1,25 +1,20 @@
 import React, {useState, useEffect} from 'react';
+import ProductDetails from '../product/ProductDetails';
 
 const CategoryCard = props => {
 
-    const [fetched3, setFetched3] = useState("")
 
-    const total = props.category.products.length()
+    const total = props.category.products.length
 
-    // const firstThree = () => {
-    //     for(i=0,i<3,i++) {
-
-    //     }
-    // }
+    const first3 = props.category.products.slice(0, 3)
 
     return (
         <>
+        
         <div>{props.category.name}</div>
         <div>{total}</div>
-        {/* {props.category.products.map(res => <div>{res.title}</div>)} */}
-        {props.category.products.map((value, index) => {
-            return <div key={index}>{value.title}</div>
-        })}
+        {first3.map(res => <div>{res.title}</div>)}
+        
         </>
     )
 
