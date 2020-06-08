6c64fd3c442583ff5f684ea210a996d80ae3e133
Tyler Simeone <tyler.simeone@gmail.com>
2020-06-08 08:27:06 -0500

    set up basic component for the home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import ApiManager from '../../modules/ApiManager'
 
 const Home = props => {
+    const [latestProducts, setLatestProducts] = useState([])
+
+    useEffect(() => {
+        ApiManager.getAll("products?total=20").then(resp => {
+            setLatestProducts(resp)
+        })
+    }, [])
 
     return(
-        <div >HOME</div>
+        <div >
+            <h1>
+                HOME
+            </h1>
+
+            {latestProducts.length === 0 ? null : (
+            <div>
+              <section>
+                {latestProducts.map(product => {
+                  return (
+                    <ProductCard
+                      key={product.id}
+                      {...props}
+                    />
+                  );
+                })}
+              </section>
+            </div>
+            )}
+        </div>
     )
 }
 
