9f7e6e246bc9377972b4c3a42a553754bec97fd2
Tyler Simeone <tyler.simeone@gmail.com>
2020-06-08 17:19:04 -0500

    added basic product styles

--- src/components/product/ProductCard.js
+++ src/components/product/ProductCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {withRouter} from 'react-router-dom'
 import ApiManager from '../../modules/ApiManager'
+import "../../styles/Product.css"
 
 const ProductCard = props => {
 
